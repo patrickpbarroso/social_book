3bbe7be316ec6af2a86050d2c96e4b07e23d71af
Patrick <patrick.plbarroso@gmail.com>
2023-03-19 19:23:36 -0300

    Like Post

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
+    path('like-post', views.like_post, name='like-post'),
 
 ]
