b081c213c46dcc8b9fa6d3cd07b4a4770490b913
Patrick <patrick.plbarroso@gmail.com>
2023-03-23 01:16:17 -0300

    Profile page

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
     path('like-post', views.like_post, name='like-post'),
-
 ]
