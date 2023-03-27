67feb07dd70496d4f0f67c796aa120e81c0df492
Patrick <patrick.plbarroso@gmail.com>
2023-03-27 08:19:17 -0300

    user search

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
     path('upload', views.upload, name='upload'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('signup', views.signup, name='signup'),
